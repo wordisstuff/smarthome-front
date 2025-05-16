d90ad202581055b05a17df90e108625bf42c4c08
wordisstuff <wordisstuff@gmail.com>
2025-05-16 16:29:27 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,88 @@
+import { useEffect } from 'react';
 import './App.css';
 import Valves from './components/Valves/Valves';
+import { getValvesStatus } from './redux/valves/operation';
+import { useDispatch, useSelector } from 'react-redux';
+import clsx from 'clsx';
+import { valvesSelector } from './redux/valves/selectors';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getValvesStatus());
+    }, [dispatch]);
+    const valves = useSelector(valvesSelector);
 
     return (
-        <>
-        <Valves/>
-        </>
+        <div className="container">
+            <div className="gg">
+                <div className="garage">Garage</div>
+                <div className="grass">
+                    Grass
+                    <Valves valveNum={1} />
+                    <span
+                        className={clsx({
+                            sprinklPos1: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                    <span
+                        className={clsx({
+                            sprinklPos2: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                    <span
+                        className={clsx({
+                            sprinklPos3: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                    <span
+                        className={clsx({
+                            sprinklPos4: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                    <span
+                        className={clsx({
+                            sprinklPos5: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                    <span
+                        className={clsx({
+                            sprinklPos6: true,
+                            sprinkl: true,
+                            sprinklOn: valves.valve1,
+                        })}
+                    >
+                        {' '}
+                    </span>
+                </div>
+            </div>
+            <div>
+                <div>Garden</div>
+                <div>Sun room</div>
+            </div>
+            <Valves valveNum={2} />
+        </div>
     );
 }
 
--- src/components/Valves/Valves.jsx
+++ src/components/Valves/Valves.jsx
@@ -1,49 +1,27 @@
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getValvesStatus, valvesTogle } from '../../redux/valves/operation';
+import { valvesTogle } from '../../redux/valves/operation';
 import { valvesSelector } from '../../redux/valves/selectors';
 import CSS from './Valve.module.css';
 
-const Valves = () => {
+const Valves = ({ valveNum }) => {
     const dispatch = useDispatch();
-
+    const key = `valve${valveNum}`;
     const valves = useSelector(valvesSelector);
     console.log('valves', valves);
-    useEffect(() => {
-        dispatch(getValvesStatus());
-    }, [dispatch]);
 
     return (
         <>
             <div className={CSS.valves}>
                 <label className={CSS.switch}>
                     {' '}
-                    v1
                     <input
                         type="checkbox"
-                        checked={valves.valve1}
+                        checked={valves[key]}
                         onChange={() =>
                             dispatch(
                                 valvesTogle({
-                                    state: !valves.valve1,
-                                    relay: 1,
-                                }),
-                            )
-                        }
-                    />
-                    <span className={CSS.slider}></span>
-                </label>
-                <label className={CSS.switch}>
-                    {' '}
-                    v2
-                    <input
-                        type="checkbox"
-                        checked={valves.valve2}
-                        onChange={() =>
-                            dispatch(
-                                valvesTogle({
-                                    state: !valves.valve2,
-                                    relay: 2,
+                                    state: !valves[key],
+                                    relay: valveNum,
                                 }),
                             )
                         }
