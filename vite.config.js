f784d1de9a14d359075550d0ef86f68ef9632f53
wordisstuff <wordisstuff@gmail.com>
2025-05-25 01:11:17 -0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg'],
+            manifest: {
+                name: 'Poliv Controller',
+                short_name: 'Poliv',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#4CAF50',
+                icons: [
+                    {
+                        src: 'smarthomeLogo.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+});
